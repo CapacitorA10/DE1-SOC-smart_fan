f764aafbf09bf088a304299292e6de96072dc245
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2020-11-18 11:51:05 +0900

    pwm을 타이머에서

--- timer_ISR.c
+++ timer_ISR.c
@@ -1,51 +1,61 @@
 #include <stdio.h>
 #include "address_map_nios2.h"
 
-extern volatile int *gpio1_ptr;//= (int*) JP1_BASE;
 extern volatile int *timer_ptr;
 extern volatile int signal_10khz;
-extern volatile int signal_1khz;
-extern volatile int signal_500hz;
-extern volatile int signal_50hz;
 extern volatile int signal_1hz;
 extern volatile int signal_0_dot_1hz;
-/*
-void delayMicroseconds(int t){
-    t *= 100; // 100Mhz에 맞게, 100을 곱하면 1us달성
-    unsigned int t_high = t >> 16;
-    unsigned int t_low = t & 0xffff;
-    *(timer_ptr) = *timer_ptr; // 타이머 초기화
-    *(timer_ptr + 5) = t_high;
-    *(timer_ptr + 4) = t_low;
-    *(timer_ptr + 1) = 0b100; // 타이머 start
+extern volatile int *gpio1_ptr;
+
+extern volatile int pwm[6]; //1000us부터 2000us까지 200us단위
+extern volatile int sensor_servo[2], fan_servo[2];
+
+void gpio_on2(int pin){
+    *gpio1_ptr |= 1 << pin; //pin에 해당하는 레지스터에 1을 추가
 }
-*/
+
+void gpio_off2(int pin){
+    *gpio1_ptr &= ~(1 << pin); //pin에 해당하는 레지스터만 0으로 하여 AND
+}
+void write_servo(int pin, int dgr){
+    switch(dgr){
+        case 0 :
+            if(pwm[0]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+        case 1 :
+            if(pwm[1]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+        case 2 :
+            if(pwm[2]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+        case 3 :
+            if(pwm[3]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+        case 4 :
+            if(pwm[4]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+        case 5 :
+            if(pwm[5]) gpio_on2(pin);
+            else  gpio_off2(pin);
+            break;
+     
+    }
+}
+
 void timer_ISR(){
     static int counter = 0;
     *timer_ptr = 0b11; //상태레지스터 초기화
     // 1회 호출될때마다 카운터는 1씩 증가
-    counter += 1;
+    counter++; //카운터는 20khz로 1씩 증가하게 됨
+
     //10khz 신호
     if(signal_10khz) signal_10khz = 0;
     else if (!signal_10khz) signal_10khz = 1;
-
-    //1khz 신호
-    if((counter % 10)==0){
-        if(signal_1khz) signal_1khz = 0;
-        else if (!signal_1khz) signal_1khz = 1;
-    }
-
-    //500hz 신호
-    if((counter % 20)==0){
-        if(signal_500hz) signal_500hz = 0;
-        else if (!signal_500hz) signal_500hz = 1;
-    }
-
-    //50hz 신호
-    if((counter % 200)==0){
-        if(signal_50hz) signal_50hz = 0;
-        else if (!signal_50hz) signal_50hz = 1;
-    }
 
     //1hz 신호
     if((counter % 10000)==0){
@@ -58,5 +68,13 @@ void timer_ISR(){
         if(signal_0_dot_1hz) signal_0_dot_1hz = 0;
         else if (!signal_0_dot_1hz) signal_0_dot_1hz = 1;
     }
+
+    for(int i = 0 ; i < 6 ; i++){
+        if( (counter % 100) < ((4 * i) + 20) ) pwm[i] = 1;    // 100으로 쪼갬 -> 0.05ms 해상도
+        else pwm[i] = 0;                                      // 20이라면 1000us만 1이고 9ms는 0
+    }                                                         // 24이라면 1200us 만 1이고 8.8ms 0
+                                                              // 40이라면 2000us 1, 8ms 0....
+    write_servo(sensor_servo[0], sensor_servo[1]);
+    write_servo(fan_servo[0], fan_servo[1]);
     if(counter >= 2000000000) counter = 0;
 }
