190832dacbfdabbf22a28b43acd949874c2af7db
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2020-11-24 14:05:15 +0900

    Added Timer
    타이머로 시간 설정하여 컷다 킬수 있슴다

--- main.c
+++ main.c
@@ -10,19 +10,22 @@ volatile int *adc1_ptr = (int*) ADC_BASE;
 volatile int *hex_5_4_ptr = (int*) HEX5_HEX4_BASE;
 volatile int *hex_3_0_ptr = (int*) HEX3_HEX0_BASE;
 volatile int *sliding_sw = (int*) SW_BASE;
+volatile int *key_ptr = (int*) KEY_BASE;
 
 unsigned int hex_nums[] = {0b00111111, 0b00000110, 0b01011011, 0b01001111, 0b01100110, 
                             0b01101101, 0b01111101, 0b00000111, 0b01111111, 0b01100111};
 
 
 int signal_10khz = 0;
+int signal_10hz = 0;
 int signal_1hz = 0;
 int signal_0_dot_1hz = 0;
 
 int counter_1hz = 0;
 int pwm[6] = {0,};
 int sensor_servo[2] = {0,}, 
-    fan_servo[2] = {0,};
+    fan_servo[2] = {0,},
+    fan_dc[2] = {0,};
 
 void gpio_on(int pin){
     *gpio1_ptr |= 1 << pin; //pin에 해당하는 레지스터에 1을 추가
@@ -58,6 +61,21 @@ void dc_mtr_on(int pin1, int pin2){ //해당 핀에 500hz로 입력을 넣음
     gpio_off(pin2);
 }
 
+void fan_dcmft(float discom, int pin2, int pin1, int clockwise){
+    int pwm_power = (int)discom;
+    if(clockwise == 1){
+        fan_dc[0] = pin1;
+        fan_dc[1] = pwm_power;
+        gpio_off(pin2);
+    }
+    else{
+        fan_dc[0] = pin2;
+        fan_dc[1] = pwm_power;
+        gpio_off(pin1);
+    }
+}
+
+
 void fan_servo_on(int pin, int loc){
     fan_servo[0] = pin;
     fan_servo[1] = loc;
@@ -89,18 +107,39 @@ int search(int servo_pin, int dist_pin){
             }
         }
     }
-    *(ledr_ptr) = prev_0_dot_1hz;
     return returnVal;
 }
 
+int analog_read(int pin){
+    *(adc1_ptr + pin) = 0; //읽기 위해 0을 써주어야 함
+    return *(adc1_ptr + pin);
+}
 
-
+void led_dcmft(int dcmft){
+    if( (dcmft > 30) && (dcmft < 55) ){
+        gpio_on(9);
+        gpio_off(11);
+        gpio_off(13);
+    }
+    else if( (dcmft >= 55) && (dcmft < 80) ){
+        gpio_on(9);
+        gpio_on(11);
+        gpio_off(13);
+    }
+    else if( (dcmft >= 80) ){
+        gpio_on(9);
+        gpio_on(11);
+        gpio_on(13);
+    }
+    
+}
 int main(void)
 {
-    float temp, temp2;
-    int prev_1hz = 0;
+    float temperature, humidity, discomfort;
+    int prev_1hz = 0, prev_1hz_ = 0, prev_10hz = 0;
     int counter_prev_1hz = 0;
     int nearist_loc = 0;
+    int timer = 0;
     // 초기화
     *ledr_ptr = 0;
     *hex_5_4_ptr = 0;
@@ -118,45 +157,78 @@ int main(void)
 
     *(gpio1_ptr + 1) = 0xffffffff; //모두 output단자로 설정
     while(1){
-        //1초마다 1씩 증가하는 카운터 구현
+        // 1초마다 1씩 증가하는 카운터 구현
         if(counter_prev_1hz != signal_1hz){
             counter_prev_1hz = signal_1hz;
             if(signal_1hz) counter_1hz++; 
         } if(counter_1hz > 100000) counter_1hz = 0; //overflow방지
 
-        //첫번째 스위치는 led on/off
-        if((*sliding_sw) & 0b1) gpio_on(0);
-        else gpio_off(0);
+        /* -------스위치가 내려가 있으면 무조건 시행, 스위치가 올라가 있으면 타이머가 1보다 커야지만 실행------- */
 
-        //두번째 스위치는 DC모터 on/off
-        if((*sliding_sw) & 0b10) dc_mtr_on(2, 3);
-        else { gpio_off(2); gpio_off(3);} //모터는 방향제어로 인한 핀 2개 소모
+        if( !((*sliding_sw) & 1) || ( ((*sliding_sw) & 1) && (timer > 0) )) { 
 
-        //적외선거리센서의 서보모터 on/off 및 최근접 점 탐색
-        if((*sliding_sw) & 0b100) nearist_loc = search(5, 0); //서보모터는 5번 gpio, 센서는 0번
+            // 두번째 스위치는 DC모터 on/off
+            if((*sliding_sw) & 0b10) fan_dcmft(0, 2, 3, 0); //dc_mtr_on(2, 3);
+            else { gpio_off(2); gpio_off(3);} //모터는 방향제어로 인한 핀 2개 소모
 
-        //모터의 서보모터 on/off 및 최근접 점 탐색
-        if((*sliding_sw) & 0b1000) fan_servo_on(7, nearist_loc); // 서보모터는 7번 gpio
-
-        //마지막으로 아날로그 읽기
-        if((*sliding_sw) & 0b10000){
-            if(signal_1hz != prev_1hz) {
-                prev_1hz = signal_1hz;
-                *adc1_ptr = 0;
-                temp = (((float) *adc1_ptr) * 0.0048828125) * 0.25; //실제 온도*100로 변환
-                temp2 = 65 * (1/temp);
-                divide_n_print((int)temp2);
-            }
-        }
-        else{
-            if(signal_1hz != prev_1hz) {
-                prev_1hz = signal_1hz;
-                *(adc1_ptr+1) = 0;
-                temp = ((float) *(adc1_ptr+1) / 4096) * 10000; //실제 습도*100로 변환
-                divide_n_print((int)temp);
+            // 서보모터 on/off 및 최근접 점 탐색하여 조준
+            if((*sliding_sw) & 0b100) {
+                nearist_loc = search(5, 0); //검색용 서보모터는 5번 gpio, 센서는 0번
+                fan_servo_on(7, nearist_loc); //선풍기용 서보모터는 7번
             }
 
+            // 마지막으로 아날로그 읽기
+            temperature = ((float) analog_read(2)) * 180 / 4096; //실제 온도로 변환(섭씨)
+            humidity = ((float) analog_read(4) / 4096) * 150 / 100; //실제 습도로 변환(1~0)
+
+            // 불쾌지수 계산
+            discomfort = (1.8 * temperature) - 0.55*(1 - humidity) * (1.8 * temperature - 26) + 32;
+            // LED 출력
+            led_dcmft(discomfort);
+
+            // HEX출력(디버깅용)
+            if(signal_1hz != prev_1hz) { 
+                prev_1hz = signal_1hz;
+                //스위치가 올라가 있으면 온도값을 hex에 표시
+                if((*sliding_sw) & 0b1000) divide_n_print((int)(temperature * 100)); //100을 곱하여 출력
+                //스위치가 내려가 있으면 습도값을 hex에 표시
+                else divide_n_print((int)(humidity * 10000));
+            }
+
+            
+        }
+        
+
+        // 타이머 기능 구현
+        if( (*sliding_sw) & 1 ) {
+            if(*key_ptr & 1){
+                if(signal_10hz != prev_10hz){
+                    prev_10hz = signal_10hz;
+                    // 0번 key를 누르고 있는 동안 타이머를 초당 20회 증가
+                    timer++;
+                }
+            }
+            else if(*key_ptr & 0b10) timer = 0; // 1번 키를 누르면 리셋
+
+
+            else{ // key를 안누르면 타이머를 초당 1씩 감소
+                if(signal_1hz != prev_1hz_){
+                    prev_1hz_ = signal_1hz;
+                    if(signal_1hz)
+                        if(timer > 0) timer--; 
+                }
+            }
+            *ledr_ptr = timer;
+            //타이머에 맞게 모터 및 서보모터 제어
         }
 
+
+
+
+
+
+        /* 실제 불쾌지수 및 거리센서 기반 선풍기 동작 */
+        //질문거리 : DE1SOC도 외부 전원 (5v 사용) 사용하여 전압 보충 가능한가?
+        //          모터 PWM을 유동적으로 하고 싶은데 어렵다.
     }
 }
