aede2043928e7d82cc57ace659faf218f9ddab8a
CapacitorA10 <53959052+CapacitorA10@users.noreply.github.com>
2020-12-13 19:45:39 +0900

    Update timer_ISR.c

--- timer_ISR.c
+++ timer_ISR.c
@@ -3,6 +3,7 @@
 
 extern volatile int *timer_ptr;
 extern volatile int signal_10khz;
+extern volatile int signal_10hz;
 extern volatile int signal_1hz;
 extern volatile int signal_0_dot_1hz;
 extern volatile int *gpio1_ptr;
@@ -47,6 +48,8 @@ void write_servo(int pin, int dgr){
     }
 }
 
+
+
 void timer_ISR(){
     static int counter = 0;
     *timer_ptr = 0b11; //상태레지스터 초기화
@@ -56,6 +59,12 @@ void timer_ISR(){
     //10khz 신호
     if(signal_10khz) signal_10khz = 0;
     else if (!signal_10khz) signal_10khz = 1;
+
+    //10hz 신호
+    if((counter % 1000)==0){
+        if(signal_10hz) signal_10hz = 0;
+        else if (!signal_10hz) signal_10hz = 1;
+    }
 
     //1hz 신호
     if((counter % 10000)==0){
@@ -70,11 +79,12 @@ void timer_ISR(){
     }
 
     for(int i = 0 ; i < 6 ; i++){
-        if( (counter % 100) < ((4 * i) + 20) ) pwm[i] = 1;    // 100으로 쪼갬 -> 0.05ms 해상도
-        else pwm[i] = 0;                                      // 20이라면 1000us만 1이고 9ms는 0
-    }                                                         // 24이라면 1200us 만 1이고 8.8ms 0
-                                                              // 40이라면 2000us 1, 8ms 0....
-    write_servo(sensor_servo[0], sensor_servo[1]);
-    write_servo(fan_servo[0], fan_servo[1]);
+        if( (counter % 400) < ((4 * i) + 20) ) pwm[i] = 1;    // 0.05ms 해상도 - 400이면 20ms 주기
+        else pwm[i] = 0;                                      // 20이라면 1000us만 1이고 19ms는 0
+    }                                                         // 24이라면 1200us 만 1이고 18.8ms 0
+                                                              // 40이라면 2000us 1, 18ms 0....
+    write_servo(sensor_servo[0], sensor_servo[1]); //센서 서보모터 돌리기
+    
+    write_servo(fan_servo[0], fan_servo[1]);       //팬 서보모터 돌리기
     if(counter >= 2000000000) counter = 0;
 }
